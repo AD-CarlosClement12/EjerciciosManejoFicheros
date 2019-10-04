61528d4e079f1f5d286801d386bf9f282821d164
CarlosClement12 <carlos.clement@iesdoctorbalmis.com>
2019-10-04 09:52:03 +0200

    Learned how to use NetBeans as a pro non scope 360 user lolxdlmao v.5 (this is embarrassing)

--- EjerciciosManejoFicheros/src/UD2Ejer901/Persona.java
+++ EjerciciosManejoFicheros/src/UD2Ejer901/Persona.java
@@ -4,6 +4,6 @@ public class Persona
 {
     public static void main(String[] args)
     {
-        System.out.println("I'm exercise 1.Persona");
+        System.out.println("I'm exercise 1.Persona ");
     }
 }
